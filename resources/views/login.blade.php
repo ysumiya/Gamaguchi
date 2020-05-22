8e7fd997f835280ea4c5343ceb94934e9f337059
“tokufumi” <tokufumi@electromonkey.net>
2020-05-22 18:27:58 +0900

    login画面を追加

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type＝”text/css” href="style.css">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+        <title>Gamaguchi</title>
+        <script>
+
+        </script>
+    </head>
+    <body>
+        <div class="login_mainfield">
+            <div class="sub_title">
+                さあ、はじめよう！
+            </div>
+            <div class="login_img">
+                <img src="img/gama.png">
+            </div>
+        </div>
+    </body>
+</html>
