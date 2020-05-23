d5e7bff75166241798749968676fb0da9546deed
ysumiya <ys95630@gmail.com>
2020-05-23 15:39:33 +0900

    public/assetsディレクトリを追加．imgとcssを移動．

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type＝”text/css” href="style.css">
+        <link rel="stylesheet" type＝”text/css” href={{ asset("assets/css/style.css") }}>
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <title>Gamaguchi</title>
         <script>
@@ -14,7 +14,7 @@
                 さあ、はじめよう！
             </div>
             <div class="login_img">
-                <img src="img/gama.png">
+                <img src={{ asset("assets/img/gama.png") }}>
             </div>
         </div>
     </body>
