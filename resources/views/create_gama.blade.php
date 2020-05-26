62a080dc3d87b90c540efbf590cf32a36458a819
tokufumi <tokufumi@electromonkey.net>
2020-05-26 15:07:23 +0900

    create_gama作成

---
+++ resources/views/create_gama.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<body class="gama_back">
+    <div class="welcome_mainbox">
+        <svg class="gamacap" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 280.29 83.84">
+            <path d="M216.51,68.84,184.22,36.56A25,25,0,0,0,140.15,13a25,25,0,0,0-44.08,23.6L63.79,68.84H15a15,15,0,0,0-15,15H70l36.67-36.67A25,25,0,0,0,140.15,37a25,25,0,0,0,33.47,10.13l36.67,36.67h70a15,15,0,0,0-15-15Z"/>
+        </svg>
+        <div class="sub_title">新しいGama</div>
+        <form action="/gama" method="post">
+            <div class="register_form">
+                <button class="gama_wrapper create_gama"><div class="gama create_gama">        
+                    <svg class="gamacap" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 280.29 83.84">
+                        <path d="M216.51,68.84,184.22,36.56A25,25,0,0,0,140.15,13a25,25,0,0,0-44.08,23.6L63.79,68.84H15a15,15,0,0,0-15,15H70l36.67-36.67A25,25,0,0,0,140.15,37a25,25,0,0,0,33.47,10.13l36.67,36.67h70a15,15,0,0,0-15-15Z"/>
+                    </svg>
+                    <i class="fas fa-5x fa-plus-circle"></i>
+                </div></button>
+                <div class="form_template">Gamaの名前
+                    <input id="gama_name" type="text" name="gama_name"　placeholder="New Gama">
+                </div>
+                    <input type="submit" class="gama_color bt2to1 button" value="作成">
+                    <form action="/home" method="post">
+                        <input type="submit" class=" bt2to1 button" value="キャンセル">
+                    </form>
+                    @crsf
+                </form>
+                @crsf
+            </div>
+    </div>
+    </body>
+@endsection
